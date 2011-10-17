aea4bc090cec1db912879dbdbdc60248fac838d2
shiba_yu36 <shibayu36@gmail.com>
2011-10-18 08:56:11 +0900

    initial commit

---
+++ lib/Test/Factory/DBI.pm
@@ -0,0 +1,48 @@
+package Test::Factory::DBI;
+
+use strict;
+use warnings;
+use Carp;
+
+our $VERSION = '0.0.1';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Test::Factory::DBI - [One line description of module's purpose here]
+
+
+=head1 SYNOPSIS
+
+    use Test::Factory::DBI;
+
+=for author to fill in:
+    Brief code example(s) here showing commonest usage(s).
+    This section will be as far as many users bother reading
+    so make it as educational and exeplary as possible.
+
+
+=head1 DESCRIPTION
+
+=for author to fill in:
+    Write a full description of the module and its features here.
+    Use subsections (=head2, =head3) as appropriate.
+
+=head1 REPOSITORY
+
+https://github.com/
+
+=head1 AUTHOR
+
+  C<< <shibayu36 {at} gmail.com> >>
+
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2011, Yuki Shibazaki C<< <shibayu36 {at} gmail.com> >>. All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
